8ba590463c6b0dc0960fd8ad3f08b45d1a223e5d
manishaa15-07 <manishachoudhary244365@gmail.com>
2024-07-13 22:40:41 +0530

    responsive_main_page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import './components/Responsive.css';
 
 function App() {
   return (
